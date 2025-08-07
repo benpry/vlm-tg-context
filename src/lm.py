bdedc4e868d2a36234ad3cb9745a3fa8c373dd42
Ben Prystawski <benpry@sc.stanford.edu>
2025-08-07 12:56:35 -0700

    fixed bugs

--- src/lm.py
+++ src/lm.py
@@ -7,12 +7,14 @@ from typing import Optional
 import pandas as pd
 import torch
 from PIL import Image
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+from qwen_vl_utils import process_vision_info
+from tqdm import tqdm
 
 from src.utils import get_image_token, preprocess_messages
 
 SYSTEM_PROMPT = """You will be presented with a list of messages between people playing a reference game, where the describer has to get the matcher to choose an image from a list of images. Your goal is to guess which of the images the describer is trying to get the matcher to choose. The images, with their labels, are shown in the image.
-
+ForCausalLM
 Please answer with just the letter corresponding to the image you think the describer is trying to get the matcher to choose.
 """
 
@@ -25,14 +27,14 @@ def get_logits(
     grid_image: Image.Image,
     n_trials: Optional[int] = None,
 ) -> list[pd.DataFrame]:
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    processor = AutoProcessor.from_pretrained(model_name)
 
     choice_token_ids = [
-        tokenizer.encode(choice, add_special_tokens=False)[0] for choice in CHOICES
+        processor.tokenizer.encode(choice, add_special_tokens=False)[0] for choice in CHOICES
     ]
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name, torch_dtype=torch.bfloat16, device_map="auto"
+    model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+        model_name, torch_dtype=torch.bfloat16, device_map="cuda"
     )
 
     return_dfs = []
@@ -42,39 +44,49 @@ def get_logits(
 
         df["chat_prompt"] = df.apply(preprocess_messages, axis=1)
 
-        prompts = []
-        for chat_prompt in df["chat_prompt"]:
-            chat = (
+        all_inputs = []
+        all_choice_logits = []
+        print("Processing prompts...")
+        for chat_prompt in tqdm(df["chat_prompt"]):
+            messages = (
                 [
                     {
                         "role": "system",
-                        "content": SYSTEM_PROMPT + get_image_token(model_name),
+                        "content": [
+                            {"type": "text", "text": SYSTEM_PROMPT},
+                            {"type": "image", "image": grid_image},
+                        ],
                     },
                     *chat_prompt,
                 ],
             )
-            print(f"chat: {chat}")
-            prompt = tokenizer.apply_chat_template(
-                chat, add_generation_prompt=True, tokenize=False
+
+            text = processor.apply_chat_template(
+                messages, add_generation_prompt=True, tokenize=False
             )[0]
-            prompts.append(
-                {
-                    "prompt": prompt,
-                    "multi_modal_data": {"image": grid_image},
-                }
+            image_inputs, _ = process_vision_info(messages)
+            inputs = processor(
+                text=[text],
+                images=image_inputs,
+                padding = True,
+                return_tensors="pt"
             )
+            inputs = inputs.to(model.device, dtype=torch.bfloat16)
 
-        out = model(
-            prompts,
-            return_dict=True,
-        )
+            with torch.inference_mode():
+                out = model(
+                    **inputs,
+                )
 
-        logits = out.logits
-        choice_logits = [logits[:, -1, tid] for tid in choice_token_ids]
 
-        df["model_logits"] = choice_logits
+            logits = out.logits
+            choice_logits = [logits[0, -1, tid].item() for tid in choice_token_ids]
+            all_choice_logits.append(choice_logits)
+
+        df["model_logits"] = all_choice_logits
 
         return_dfs.append(
+
             df[
                 [
                     "trial_id",
